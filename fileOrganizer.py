318c18dd260af67bfdec35ca118e1f70626503e9
jchionak <jchionak@icloud.com>
2023-07-16 15:02:18 -0400

    comments

--- fileOrganizer.py
+++ fileOrganizer.py
@@ -9,13 +9,14 @@ path = input('Enter path: ')
 files = os.listdir(path)
 
 for file in files:
+    # splits files into name and extension
     filename, extension = os.path.splitext(file)
     extension = extension[1:]
 
-    if os.path.exists(path + '/' + extension):
+    if os.path.exists(path + '/' + extension):  # if a folder with this filename exists, add it there
         shutil.move(path + '/' + file, path + '/' + extension + '/' + file)
     else:
-        os.makedirs(path + '/' + extension)
+        os.makedirs(path + '/' + extension)  # create a new folder if it doesn't exist
         shutil.move(path + '/' + file, path + '/' + extension + '/' + file)
 
 print('Done organizing!')
